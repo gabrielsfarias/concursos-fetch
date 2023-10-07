cb9e8318fb496d057a61681bfbf6d4cd20527aad
Gabriel Souza <gabrielsfarias@outlook.com>
2023-10-07 01:33:45 -0300

    correção

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { crawlerCesgranrio } from './bot/cesgranrio.js'
-import { crawlerFgv } from './bot/fgv'
+import { crawlerFgv } from './bot/fgv.js'
 
 export default { crawlerFgv, crawlerCesgranrio }
